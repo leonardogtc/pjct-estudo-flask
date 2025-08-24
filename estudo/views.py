6f2b9f90b9f6d45ffe7c11a3e5e716522a1d7273
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2025-08-24 08:53:48 -0300

    Trabalhando com rotas com a biblioteca url_for

--- estudo/views.py
+++ estudo/views.py
@@ -1,5 +1,5 @@
 from estudo import app
-from flask import render_template
+from flask import render_template, url_for
 
 # Para importar a página homepage do arquivo view.py do pacote estudo.
 
@@ -7,6 +7,11 @@ from flask import render_template
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+
+@app.route('/sobre')
+def novapage():
+    return "nova página"
 
 
 if __name__ == '__main__':
